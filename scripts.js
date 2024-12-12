5d2f3973a719d27b69f96bc8fff83e840d24f877
Oisau2000 <112674833+Oisau2000@users.noreply.github.com>
2024-12-12 14:03:24 -0800

    Add files via upload

--- scripts.js
+++ scripts.js
@@ -22,7 +22,14 @@ function showRandomFact() {
         "A group of cats is called a clowder.",
         "Cats can make over 100 different sounds.",
         "A cat’s nose is as unique as a human fingerprint.",
-        "Cats spend 70% of their lives sleeping."
+        "Cats spend 70% of their lives sleeping.",,
+        "Cats have a special reflective layer behind their retinas, called the tapetum lucidum, which helps them see in low light.",
+        "A cat's whiskers are not just for measuring gaps; they also help them navigate in the dark.",
+        "Domestic cats can run up to 30 miles per hour in short bursts.",
+        "Cats have been domesticated for over 4,000 years.",
+        "Cats can jump up to six times their body length in one leap.",
+        "A cat’s heart beats twice as fast as a human’s, at 110-140 beats per minute.",
+        "The oldest recorded cat lived to be 38 years and 3 days old.",
     ];
 
     const randomFact = facts[Math.floor(Math.random() * facts.length)];
@@ -32,7 +39,6 @@ function showRandomFact() {
     }
 }
 
-// Run functions after the page loads
 window.onload = function () {
     displayGreeting();
     showRandomFact();
